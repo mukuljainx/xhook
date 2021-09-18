c33c669cd6780a5a4b51f1b4c25d78f8a74d03c3
Mukul Jain <jainmukul1996@gmail.com>
2021-09-19 01:03:19 +0530

    chore: housekeeping

---
+++ dev/fetch.ts
@@ -0,0 +1,16 @@
+const api = (URL) => {
+  const options = {
+    headers: {
+      Accept: "application/json",
+      card: "111",
+    },
+  };
+
+  console.log("before", options.headers);
+
+  fetch(URL.get, options).then(() => {
+    console.log("after", options.headers);
+  });
+};
+
+export default api;
---
+++ dev/index.ts
@@ -0,0 +1,40 @@
+// import axios from "axios";
+import f from "../dist/xhook";
+console.log(99, f.xhook);
+const xhook = f.xhook;
+
+xhook.before((req, callback) => {
+  console.log("XHOOK BEFORE");
+  console.log(req);
+  console.log("\n\n");
+  callback();
+});
+
+xhook.after(function (request, originalResponse) {
+  console.log("XHOOK AFTER");
+  console.log({ request, originalResponse });
+  console.log("\n\n");
+});
+
+const URL = {
+  get: "http://demo5468585.mockable.io/patients/empi/goals",
+  post: "https://demo5468585.mockable.io/events",
+};
+
+// axios
+//   .get(URL.get)
+//   .then((response) => {
+//     console.log({ data: response.data, headers: response.headers });
+//   });
+
+const form = new FormData();
+form.append("name", "test");
+
+fetch(URL.post, {
+  method: "post",
+  body: form,
+}).then((d) => {
+  console.log(433, d);
+});
+
+console.log(form);
